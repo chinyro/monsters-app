7ac4c7450cb48b6a1f8ff85c059d30f8b78cbb6c
sydrob <55166840+sydrobhez@users.noreply.github.com>
2021-09-05 23:35:54 -0500

    updating packages

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Card = ({username, email, id}) => {
     return (
-        <div className='tc bg-mid-gray br3 pa3 ma2 dib grow bw2 shadow-5 pointer'>
+        <div className='tc bg-light-blue br3 pa3 ma2 dib grow bw2 shadow-5 pointer'>
           <img alt="monsters" src={`https://robohash.org/${id}?set=set2`} />  
           <div>
               <h2 className='washed-yellow f2 avenir'>{username}</h2>
-              <p className='light-blue f5'>{email}</p>
+              <p className='dark-blue f4'>{email}</p>
           </div>
         </div>
     )
